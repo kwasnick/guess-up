d051ebbde3248e81d99638188c827aba3fe34986
Daniel Kwasnick <kwasnick@gmail.com>
2025-07-25 10:41:47 -0700

    fix replays

--- tilt-detector.js
+++ tilt-detector.js
@@ -2,11 +2,15 @@ export class TiltDetector extends EventTarget {
   /**
    * @param {object} options
    * @param {number} [options.threshold=50] Angle offset from 90° to detect up/down
+   * @param {number} [options.resetThreshold=threshold/2] Angle offset from 90° to reset back to neutral
    * @param {number} [options.frequency=60] Sensor update frequency (Hz)
    */
-  constructor({ threshold = 50, frequency = 60 } = {}) {
+  constructor({ threshold = 50, resetThreshold = 25, frequency = 60 } = {}) {
     super();
     this.threshold = threshold;
+    // If resetThreshold isn't provided, default to half of threshold
+    this.resetThreshold =
+      resetThreshold !== undefined ? resetThreshold : threshold / 2;
     this.frequency = frequency;
     this.sensor = null;
     this._tiltState = "neutral";
@@ -44,28 +48,49 @@ export class TiltDetector extends EventTarget {
   }
 
   _onReading() {
-    const [x, y, ,] = this.sensor.quaternion;
+    const [x, y] = this.sensor.quaternion;
 
     // world‑Z of device’s +Z axis
     const zCompRaw = 1 - 2 * (x * x + y * y);
     const zComp = Math.min(1, Math.max(-1, zCompRaw));
     const angleDeg = (Math.acos(zComp) * 180) / Math.PI;
 
-    let newState = "neutral";
-    if (angleDeg > 90 + this.threshold) newState = "down";
-    else if (angleDeg < 90 - this.threshold) newState = "up";
+    // difference from 90°
+    const diff = angleDeg - 90;
+    let newState;
+
+    switch (this._tiltState) {
+      case "neutral":
+        if (diff > this.threshold) newState = "down";
+        else if (diff < -this.threshold) newState = "up";
+        else newState = "neutral";
+        break;
+
+      case "up":
+        // require coming closer to neutral by resetThreshold before resetting
+        if (diff > -this.resetThreshold) newState = "neutral";
+        else newState = "up";
+        break;
+
+      case "down":
+        // require coming closer to neutral by resetThreshold before resetting
+        if (diff < this.resetThreshold) newState = "neutral";
+        else newState = "down";
+        break;
+
+      default:
+        newState = "neutral";
+    }
 
     if (newState !== this._tiltState) {
       this._tiltState = newState;
+      const eventMap = {
+        up: "tiltup",
+        down: "tiltdown",
+        neutral: "neutral",
+      };
       this.dispatchEvent(
-        new CustomEvent(
-          {
-            up: "tiltup",
-            down: "tiltdown",
-            neutral: "neutral",
-          }[newState],
-          { detail: { angle: angleDeg } }
-        )
+        new CustomEvent(eventMap[newState], { detail: { angle: angleDeg } })
       );
     }
   }
